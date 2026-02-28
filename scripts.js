d5b53ae9230a76f66aa0edc4e836d69cca94fd4c
Jay Zhong <jay.zhong99@gmail.com>
2026-02-28 11:19:28 +1100

    added a ground to the scene

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ class RocketSimulator {
     constructor() {
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer();
-        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth, window.innerHeight, 1, 1000);
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
         this.init();
     }
     init() {
@@ -12,12 +12,32 @@ class RocketSimulator {
         document.body.appendChild(this.renderer.domElement);
         this.scene.background = new THREE.Color(0x0000ff);
         this.scene.add(this.camera);
+
+        this.camera.position.set(20, 10, 20);
+        this.camera.lookAt(0, 12, 0);
+
+        const directionalLight = new THREE.AmbientLight(0xffffff, 1);
+        directionalLight.position.set(100, 200, 100);
+        this.scene.add(directionalLight);
+
+        this.ground = new Ground(0x00ff00);
+        this.scene.add(this.ground.mesh);
         this.animate = this.animate.bind(this);
         this.animate();
     }
     animate() {
         requestAnimationFrame(this.animate);
         this.renderer.render(this.scene, this.camera);
+    }
+}
+
+class Ground {
+    constructor(color) {
+        this.geometry = new THREE.PlaneGeometry(1400, 1400);
+        this.material = new THREE.MeshLambertMaterial({ color: color});
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.rotation.x = -Math.PI / 2;
+        this.mesh.position.set(0, 0, 0);
     }
 }
 
