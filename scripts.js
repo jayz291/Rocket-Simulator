6fa81513bb0e61c05d8f3f5298db58b6149d4370
Jay Zhong <jay.zhong99@gmail.com>
2026-03-02 23:35:57 +1100

    added stat display, added reset logic, camera now moves by deltaY (as opposed to being fixed to the rocket Mesh y-value, and lighting is now directional

--- scripts.js
+++ scripts.js
@@ -2,16 +2,21 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const simulationBox = document.getElementById('simulation');
+const altitudeStat = document.getElementById('altitude');
+const velocityStat = document.getElementById('velocity');
+const timeStat = document.getElementById('time');
 const G = 6.6743e-11;
 
 class RocketSimulator {
     constructor(simulation) {
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true});
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.camera = new THREE.PerspectiveCamera(45, 
             simulationBox.clientWidth / simulationBox.clientHeight, 1, 10000000);
-        this.init();
         this.simulation = simulation;
+        this.init();
     }
     init() {
         this.renderer.setSize(simulationBox.clientWidth, simulationBox.clientHeight);
@@ -27,28 +32,47 @@ class RocketSimulator {
         this.camera.position.set(20, 10, 20);
         this.camera.lookAt(0, 12, 0);
 
-        const directionalLight = new THREE.AmbientLight(0xffffff, 1);
-        directionalLight.position.set(100, 200, 100);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+        directionalLight.position.set(20000, 20000, 300);
         this.scene.add(directionalLight);
 
         this.ground = new Ground(0x00ff00);
+        this.launchPad = new LaunchPad();
         this.rocket = new Rocket(3, 2);
         this.scene.add(this.ground.mesh);
         this.scene.add(this.rocket.mesh);
+        this.scene.add(this.launchPad.mesh);
         this.simulating = false;
+        this.resetting = false;
         this.animate = this.animate.bind(this);
         this.animate();
     }
     animate() {
         requestAnimationFrame(this.animate);
         if (this.simulating && this.simulation && !this.simulation.finished) {
+            let previousHeight = this.simulation.currentHeight;
             this.simulation.updatePhysics(0.016);
+            let deltaY = this.simulation.currentHeight - previousHeight;
             this.rocket.mesh.position.y = 8 + this.simulation.currentHeight;
-            this.camera.position.y = 8 + this.simulation.currentHeight;
+            this.camera.position.y += deltaY;
             this.controls.target.y = this.rocket.mesh.position.y;
+            this.controls.maxPolarAngle = Math.PI;
         } else {
             this.simulating = false;
         }
+        if (this.resetting) {
+            this.simulation.currentHeight = 0;
+            this.simulation.velocity = 0;
+            this.simulation.time = 0;
+            this.simulation.fuelMass = this.simulation.originalFuelMass;
+            this.rocket.mesh.position.y = 8 + this.simulation.currentHeight;
+            this.camera.position.y = 8 + this.simulation.currentHeight;
+            this.controls.target.y = this.rocket.mesh.position.y;
+            this.resetting = false;
+        }
+        altitudeStat.textContent = this.simulation.currentHeight.toFixed(5);
+        velocityStat.textContent = this.simulation.velocity.toFixed(5);
+        timeStat.textContent = this.simulation.time.toFixed(2);
         this.controls.update();
         this.renderer.render(this.scene, this.camera);
     }
@@ -56,11 +80,21 @@ class RocketSimulator {
 
 class Ground {
     constructor(color) {
-        this.geometry = new THREE.PlaneGeometry(1400, 1400);
+        this.geometry = new THREE.PlaneGeometry(140000, 140000);
         this.material = new THREE.MeshLambertMaterial({ color: color});
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.rotation.x = -Math.PI / 2;
         this.mesh.position.set(0, 0, 0);
+    }
+}
+
+class LaunchPad {
+    constructor() {
+        this.geometry = new THREE.PlaneGeometry(10, 10);
+        this.material = new THREE.MeshLambertMaterial({color: 0x808080});
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.rotation.x = -Math.PI / 2;
+        this.mesh.position.set(0, 0.1, 0);
     }
 }
 
@@ -109,10 +143,11 @@ class Simulation {
         this.atmosphereThickness = 100;
         this.airDensity = 1.225;
         this.scaleHeight = 8.5 * 1000;
-        this.rocketRadius = 2;
+        this.rocketRadius = 0.1764;
         this.rocketMass = 10;
-        this.fuelMass = 5;
-        this.fuelConsumptionRate = 1;
+        this.fuelMass = 8;
+        this.originalFuelMass = this.fuelMass;
+        this.fuelConsumptionRate = 0.8;
         this.thrustForce = 500;
         this.currentHeight = 0;
         this.velocity = 0;
@@ -132,6 +167,7 @@ class Simulation {
         this.rocketRadius = data.rocketRadius;
         this.rocketMass = data.rocketMass;
         this.fuelMass = data.fuelMass;
+        this.originalFuelMass = data.fuelMass;
         this.fuelConsumptionRate = data.fuelConsumptionRate;
         this.thrustForce = data.thrustForce;
 
@@ -157,11 +193,11 @@ class Simulation {
             //console.log(G * this.planetMass);
             //console.log((this.planetRadius + this.currentHeight) ** 2);
             if (this.fuelMass > 0) {
-                force = this.thrustForce - gravity * this.totalMass - 
+                force = this.thrustForce - gravity * this.totalMass - /*0.5 **/
                 currentAirDensity * this.crossSectionalArea * (this.velocity ** 2) * Math.sign(this.velocity);
             } else {
-                this.totalMass = this.rocketMass;
-                force = -gravity * this.totalMass -
+                //this.totalMass = this.rocketMass;
+                force = -gravity * this.totalMass - /*0.5 **/
                 currentAirDensity * this.crossSectionalArea * (this.velocity ** 2) * Math.sign(this.velocity);
             }
             //console.log(this.totalMass);
@@ -174,7 +210,7 @@ class Simulation {
             this.time += deltaTime;
             console.log(currentAirDensity);
 
-            if (this.velocity > this.escapeVelocity) {
+            if (this.velocity > this.escapeVelocity && this.currentHeight > 10000000) {
                 this.finished = true;
             }
             if (this.time > 2 && this.currentHeight == 0) {
@@ -191,6 +227,7 @@ const rocketSimulator = new RocketSimulator(currentSimulation);
 
 const updateButton = document.getElementById("configure-btn");
 const startButton = document.getElementById("startButton");
+const resetButton = document.getElementById("resetButton");
 
 updateButton.addEventListener('click', () => {
     const inputData = {
@@ -207,9 +244,21 @@ updateButton.addEventListener('click', () => {
         thrustForce: parseFloat(document.getElementById('thrustForce').value)
     }
     currentSimulation.updateStats(inputData);
+    rocketSimulator.resetting = true;
 });
 
 startButton.addEventListener('click', () => {
     currentSimulation.finished = false;
     rocketSimulator.simulating = true;
+});
+
+resetButton.addEventListener('click', () => {
+
+    currentSimulation.finished = true;
+    rocketSimulator.simulating = false;
+    rocketSimulator.resetting = true;
+  
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+
 });
